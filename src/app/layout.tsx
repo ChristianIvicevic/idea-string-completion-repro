96e210564d7aac44c0952800b5a3388f25a0182f
Christian Ivicevic <mail@christian-ivicevic.com>
2025-04-06 11:20:01 +1000

    Update repro

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,10 +6,20 @@ export const dynamic = 'force-dynamic'
 type Union = 'foo' | 'bar' | 'boo' | 'baz'
 function f(value: Union) {}
 
+declare function myGetTranslations(): {
+	<TargetKey extends Union>(key: TargetKey): string
+	rich<TargetKey extends Union>(key: TargetKey): ReactNode
+}
+
 export default async function RootLayout({ children }: { children: ReactNode }) {
 	const locale = await getLocale()
 
 	const t = await getTranslations()
+	const t2 = myGetTranslations()
+
+	f('')
+	t('')
+	t2('')
 
 	return (
 		<html lang={locale}>
